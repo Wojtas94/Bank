6052244ff8f3aa80efcb17a00064cb9bac0cac90
Wojciech Misiaszek <wojciechmisiaszek94@gmail.com>
2019-05-14 18:29:11 +0200

    ssasdd

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -3,8 +3,13 @@ public class Bank {
     public static void main(String[] args) {
         CompanyAccount ca = new CompanyAccount(1122222, 2233333);
         ca.depositOrWithdraw();
+
         ca.open("Aleksander", 22233, "stan");
+<<<<<<< HEAD
         ca.toString();
+=======
+        ca.open("Aleksande33r", 22233, "stan");
+>>>>>>> develop
         PersonalAccount pa = new PersonalAccount();
         pa.downloadType();
         pa.downloadType();
